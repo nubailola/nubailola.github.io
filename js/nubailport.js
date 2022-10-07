f7d4fbb418512b92cfabfa857343900579be47b0
nubailola <115222142+nubailola@users.noreply.github.com>
2022-10-07 11:11:58 +0100

    ddd
    ddd

---
+++ js/nubailport.js
@@ -0,0 +1,8 @@
+/* ==================== typing animation ===================*/
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Solar System Installer","3D Flooring & Wall Epoxy Installer"],
+    typespeed:-50,
+    Backspeed:-20,
+    loop:true
+})
+
